c936eb425463823f66660d097dfd91712acb4331
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-05-22 15:38:26 -0400

    completed lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,58 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+ # this is a method that detaches creation from the initialize method
+  def self.create
+    @@all << self.new
+    @@all.last
+  end
+
+  # allows me to instantiate new objects by a name
+  def self.new_by_name(name)
+    person = self.new
+    person.name = name
+    @@all << person
+    @@all.last
+  end
+
+
+  def self.create_by_name(name)
+    self.new_by_name(name)
+  end
+
+  # allows me to find a song by using a name
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  # determines if a it can find a song if not it creates a new instance using a name param
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) == nil ? self.create_by_name(name) : self.find_by_name(name)
+
+  end
+
+  # sorts the all array by key value using the &:shorcut
+  def self.alphabetical
+    self.all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(file_data)
+    data = file_data.split(" - ")
+    artist_name = data[0]
+    song_name = data[1].split(".")[0]
+    song = self.create_by_name(song_name)
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(file_data)
+    self.new_from_filename(file_data)
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
